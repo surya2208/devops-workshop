38b9868dd42d13edd38fd7fbf76560aa3527c370
Surya <suryaforwin@gmail.com>
2026-05-15 19:49:27 +0530

    added v3 version

---
+++ terraform_code/V3-EC2-With_VPC.tf
@@ -0,0 +1,97 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "demo-server3" {
+  ami           = "ami-091138d0f0d41ff90"
+  instance_type = "t3.micro"
+  key_name      = "surya-key"
+  vpc_security_group_ids = [aws_security_group.demo-sg.id]
+  subnet_id = aws_subnet.dpp-public-subnet-01.id
+
+}
+
+resource "aws_security_group" "demo-sg" {
+  name        = "demo-sg"
+  description = "SSH Access"
+  vpc_id = aws_vpc.dpp-vpc.id
+
+  ingress {
+    description = "SSH access"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "ssh-prot"
+
+  }
+}
+
+resource "aws_vpc" "dpp-vpc" {
+  cidr_block = "10.1.0.0/16"
+  tags = {
+    Name = "dpp-vpc"
+  }
+}
+
+resource "aws_subnet" "dpp-public-subnet-01" {
+  vpc_id            = aws_vpc.dpp-vpc.id
+  cidr_block        = "10.1.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone = "us-east-1a"
+  tags = {
+    Name = "dpp-public-subnet-01"
+  }
+}
+
+
+resource "aws_subnet" "dpp-public-subnet-02" {
+  vpc_id            = aws_vpc.dpp-vpc.id
+  cidr_block        = "10.1.2.0/24"
+  map_public_ip_on_launch = true
+  availability_zone = "us-east-1b"
+  tags = {
+    Name = "dpp-public-subnet-02"
+  }
+}
+
+resource "aws_internet_gateway" "dpp-igw" {
+  vpc_id = aws_vpc.dpp-vpc.id
+  tags = {
+    Name = "dpp-igw"
+  }
+}
+
+resource "aws_route_table" "dpp-public-rt" {
+  vpc_id = aws_vpc.dpp-vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.dpp-igw.id
+  }
+  tags = {
+    Name = "dpp-public-rt"
+  }
+}
+
+resource "aws_route_table_association" "dpp-public-subnet-01-association" {
+  subnet_id      = aws_subnet.dpp-public-subnet-01.id
+  route_table_id = aws_route_table.dpp-public-rt.id
+  
+}
+
+resource "aws_route_table_association" "dpp-public-subnet-02-association" {
+  subnet_id      = aws_subnet.dpp-public-subnet-02.id
+  route_table_id = aws_route_table.dpp-public-rt.id
+  
+}
