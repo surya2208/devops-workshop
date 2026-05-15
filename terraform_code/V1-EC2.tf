8545cdec570d8b714f102c1a9d86cd1950fa9cf2
Surya <suryaforwin@gmail.com>
2026-05-15 18:12:46 +0530

    starting from scracth and commiting terraform files

--- terraform_code/V1-EC2.tf
+++ terraform_code/V1-EC2.tf
@@ -3,7 +3,34 @@ provider "aws" {
 }
 
 resource "aws_instance" "demo-server" {
-    ami = "ami-022e1a32d3f742bd8"
-    instance_type = "t2.micro"
-    key_name = "dpp"
+  ami           = "ami-0cca150d127c2216f"
+  instance_type = "t3.micro"
+  key_name      = "surya-key"
+  security_groups = [ "demo-sg" ]
+}
+
+resource "aws_security_group" "demo-sg" {
+  name        = "demo-sg"
+  description = "SSH Access"
+
+  ingress {
+    description = "SSH access"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "ssh-prot"
+
+  }
 }
